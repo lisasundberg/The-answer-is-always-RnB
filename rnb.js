bf3784828130db6021efe06b25357124ac3d8ee3
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2018-03-08 14:29:37 +0100

    Add event listener on input field to show tracks on change

--- rnb.js
+++ rnb.js
@@ -1,27 +1,32 @@
 const apiKey = '81eb011ebdea34eb07af4188c24c4eb9';
 const method = 'user.getlovedtracks';
 const user = 'ultralisan';
+const input = document.getElementById('input');
 
 
-(async(username) => {
-	try {
-		var response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=ultralisan&api_key=81eb011ebdea34eb07af4188c24c4eb9&format=json`);
-		var json = await response.json();
-//		var lovedTracks = json.map(lovedtracks => lovedtracks.track.name);
-		
-		//Pick out all the loved track-names and add them to a separate array
-		const lovedTracks = json.lovedtracks.track;
-		const lovedTracksNames = [];
-		for(var track of lovedTracks) {
-			lovedTracksNames.push(track.name);
+input.addEventListener('change', function(){
+
+	(async(username) => {
+		try {
+			var response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=ultralisan&api_key=81eb011ebdea34eb07af4188c24c4eb9&format=json`);
+			var json = await response.json();
+	//		var lovedTracks = json.map(lovedtracks => lovedtracks.track.name);
+
+			//Pick out all the loved track-names and add them to a separate array
+			const lovedTracks = json.lovedtracks.track;
+			const lovedTracksNames = [];
+			for(var track of lovedTracks) {
+				lovedTracksNames.push(track.name);
+			}
+			console.log(lovedTracksNames);
+		}	catch(e) {
+			console.log("Data didn't load", e);
 		}
-		console.log(lovedTracksNames);
-	}	catch(e) {
-		console.log("Data didn't load", e);
-	}
-})
-//(user);
+	})(user);
 
+});
+
+//Math.floor((Math.random() * 100) + 1);
 
 //function getLastFmData() {
 //	fetch(`https://ws.audioscrobbler.com/2.0/?method=${method}&user=${user}api_key=${apiKey}&format=json`)
