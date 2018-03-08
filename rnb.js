fd312572550dbd6f7ec0a4dc79189267a5b1faf0
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2018-03-08 13:11:16 +0100

    Get loved tracks from Last FM

---
+++ rnb.js
@@ -0,0 +1,52 @@
+const apiKey = '81eb011ebdea34eb07af4188c24c4eb9';
+const method = 'user.getlovedtracks';
+const user = 'ultralisan';
+
+
+(async(username) => {
+	try {
+		var response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=ultralisan&api_key=81eb011ebdea34eb07af4188c24c4eb9&format=json`);
+		var json = await response.json();
+//		var lovedTracks = json.map(lovedtracks => lovedtracks.track.name);
+		
+		const lovedTracks = json.lovedtracks.track;
+		
+		const lovedTracksNames = [];
+		
+		for(var track of lovedTracks) {
+			lovedTracksNames.push(track.name);
+		}
+	}	catch(e) {
+		console.log("Data didn't load", e);
+	}
+})(user);
+
+
+//function getLastFmData() {
+//	fetch(`https://ws.audioscrobbler.com/2.0/?method=${method}&user=${user}api_key=${apiKey}&format=json`)
+//	//as soon as the fetch has resolved, 
+//	//take the response and turn it into JSON
+//    .then((response) => response.json())
+//    .then(function(lastFmData){
+//		displayLastFmData(lastFmData);
+//    })
+//    .catch(function(error){
+//      console.log(error);
+//    })
+//}
+//
+//
+//function displayLastFmData(lastFmData){
+//	const { lovedtracks } = lastFmData;
+//	
+//	const lastFmInfoElement = document.getElementById('lastFmInfo');
+//	let lastFmInfo = `
+//		<p>${user} loves</p>
+//		<p> ${lovedtracks.track.name}</p>
+//		<p> ${lovedtracks.track.artist.name}</p>
+//  `;
+//
+//  lastFmInfoElement.innerHTML = lastFmInfo;
+//}
+
+
