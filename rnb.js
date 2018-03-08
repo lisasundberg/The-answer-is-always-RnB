cc16aee955ed51944943d9c261bff577da3ec98b
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2018-03-08 16:43:12 +0100

    Randomize one song and print out

--- rnb.js
+++ rnb.js
@@ -12,14 +12,8 @@ input.addEventListener('change', function(){
 		try {
 			var response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=ultralisan&api_key=81eb011ebdea34eb07af4188c24c4eb9&format=json`);
 			var json = await response.json();
-	//		var lovedTracks = json.map(lovedtracks => lovedtracks.track.name);
-
-			//Pick out all the loved track-names and add them to a separate array
 			lovedTracks = json.lovedtracks.track;
-
-//			for(var track of lovedTracks) {
-//				lovedTracksNames.push(track.name);
-//			}
+			
 			displayTracks(lovedTracks);
 			
 		}	catch(e) {
@@ -27,58 +21,31 @@ input.addEventListener('change', function(){
 		}
 	})(user);
 
-
-//	const outputDiv = document.getElementById('output');
-//	let outputText = `
-//	<p>The answer is</p>
-//	<p> ${lovedTracks.name[0]}</p>
-//`;
-
-//output.innerHTML = outputText;
-
 });
-
-function displayTracks(tracks){
-	const output = document.getElementById('output');
-	
-	let htmlBlock = "";
-	
-	for(const track of tracks){
-		console.log(track.name);
-		htmlBlock += `<p>${track.name}</p>`;
-	}
-	output.insertAdjacentHTML('afterbegin', htmlBlock);
-}
-
 
 
 //Math.floor((Math.random() * 100) + 1);
 
-//function getLastFmData() {
-//	fetch(`https://ws.audioscrobbler.com/2.0/?method=${method}&user=${user}api_key=${apiKey}&format=json`)
-//	//as soon as the fetch has resolved, 
-//	//take the response and turn it into JSON
-//    .then((response) => response.json())
-//    .then(function(lastFmData){
-//		displayLastFmData(lastFmData);
-//    })
-//    .catch(function(error){
-//      console.log(error);
-//    })
-//}
-//
-//
-//function displayLastFmData(lastFmData){
-//	const { lovedtracks } = lastFmData;
-//	
-//	const lastFmInfoElement = document.getElementById('lastFmInfo');
-//	let lastFmInfo = `
-//		<p>${user} loves</p>
-//		<p> ${lovedtracks.track.name}</p>
-//		<p> ${lovedtracks.track.artist.name}</p>
-//  `;
-//
-//  lastFmInfoElement.innerHTML = lastFmInfo;
-//}
+var randomTrack = lovedTracks[Math.floor(Math.random()*lovedTracks.length)];
+
+function displayTracks(tracks){
+	const output = document.getElementById('output');
+	let randomTrack = lovedTracks[Math.floor(Math.random()*lovedTracks.length)];
+	
+	const htmlBlock = `
+		<p>${randomTrack.name}</p>
+		<p>${randomTrack.artist.name}</p>
+	`;
+	output.innerHTML = htmlBlock;
+	
+	//Prints out all the tracks
+	//	for(const track of tracks){
+	////		console.log(track.name);
+	////		htmlBlock += `<p>${track.name}</p>`;
+	//	}
+}
+
+
+
 
 
