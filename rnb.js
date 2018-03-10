ef2014721b323cb2a9cbcae83786e237d648af13
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2018-03-10 14:30:51 +0100

    Add delay-function

--- rnb.js
+++ rnb.js
@@ -1,29 +1,31 @@
 const apiKey = '81eb011ebdea34eb07af4188c24c4eb9';
 const method = 'user.getlovedtracks';
 const user = 'ultralisan';
+const url = `https://ws.audioscrobbler.com/2.0/?method=${method}&user=${user}&api_key=${apiKey}&format=json`;
+
 const input = document.getElementById('input');
-let lovedTracks = [];
-//const lovedTracksNames = [];
 const output = document.getElementById('output');
+
+let lovedTracks = [];
+
+
+const getLovedTracks = async (username) => {
+	try {
+		await delay(1);
+		var response = await fetch(url);
+		var json = await response.json();
+		lovedTracks = json.lovedtracks.track;
+		displayTracks(lovedTracks);
+	}	catch(error) {
+		console.log("Data didn't load", error);
+	}
+}
+
 
 input.addEventListener('change', function(){
 	getLovedTracks(user);
 });
 
-async function getLovedTracks(username) {
-	try {
-		var response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=ultralisan&api_key=81eb011ebdea34eb07af4188c24c4eb9&format=json`);
-		var json = await response.json();
-		lovedTracks = json.lovedtracks.track;
-		displayTracks(lovedTracks);
-
-	}	catch(e) {
-		console.log("Data didn't load", e);
-	}
-}
-
-//Select a random track
-var randomTrack = lovedTracks[Math.floor(Math.random()*lovedTracks.length)];
 
 function displayTracks(tracks){
 	
@@ -45,14 +47,15 @@ function displayTracks(tracks){
 //	const loader = `<div class="loader" id="loader"></div>`;
 //	output.innerHTML = loader;
 	output.innerHTML = htmlBlock;
-	
-	//Prints out all the tracks
-	//	for(const track of tracks){
-	////		console.log(track.name);
-	////		htmlBlock += `<p>${track.name}</p>`;
-	//	}
 }
 
+
+//Create a function to delay displaying the information
+function delay(seconds) {
+	return new Promise(
+	resolve => setTimeout(resolve, seconds * 1000)
+	)
+};
 
 //<blockquote class="embedly-card"><h4><a href="https://www.last.fm/music/Jhen%C3%A9+Aiko/_/Sativa">Jhené Aiko - Sativa</a></h4><p>Watch the video for Sativa by Jhené Aiko for free, and see the artwork, lyrics and similar artists.</p></blockquote>
 //<script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
