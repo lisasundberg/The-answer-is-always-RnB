175e1a4c229572ce6a28f8f3ef4e5972d03a49af
lisasundberg <6d:b7:47:1a:d1:2a:bf:ea:19:11:30:9b:5a:d3:d7:a1>
2018-03-10 13:31:35 +0100

    Move fetch-function outside of eventlistener

--- rnb.js
+++ rnb.js
@@ -4,33 +4,34 @@ const user = 'ultralisan';
 const input = document.getElementById('input');
 let lovedTracks = [];
 //const lovedTracksNames = [];
-
+const output = document.getElementById('output');
 
 input.addEventListener('change', function(){
-
-	(async(username) => {
-		try {
-			var response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=ultralisan&api_key=81eb011ebdea34eb07af4188c24c4eb9&format=json`);
-			var json = await response.json();
-			lovedTracks = json.lovedtracks.track;
-			
-			displayTracks(lovedTracks);
-			
-		}	catch(e) {
-			console.log("Data didn't load", e);
-		}
-	})(user);
-
+	getLovedTracks(user);
 });
+
+async function getLovedTracks(username) {
+	try {
+		var response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=ultralisan&api_key=81eb011ebdea34eb07af4188c24c4eb9&format=json`);
+		var json = await response.json();
+		lovedTracks = json.lovedtracks.track;
+		displayTracks(lovedTracks);
+
+	}	catch(e) {
+		console.log("Data didn't load", e);
+	}
+}
 
 //Select a random track
 var randomTrack = lovedTracks[Math.floor(Math.random()*lovedTracks.length)];
 
 function displayTracks(tracks){
-	const output = document.getElementById('output');
+	
 	let randomTrack = lovedTracks[Math.floor(Math.random()*lovedTracks.length)];
 	
 	const htmlBlock = `
+		<small>You asked</small>
+		<p class="question">${input.value}</p>
 		<div class="image">
 			<img src="${randomTrack.image[3]['#text']}" alt="${randomTrack.artist.name}">
 		</div>
@@ -41,6 +42,8 @@ function displayTracks(tracks){
 			<p>${randomTrack.artist.name}</p>
 		</a>
 	`;
+//	const loader = `<div class="loader" id="loader"></div>`;
+//	output.innerHTML = loader;
 	output.innerHTML = htmlBlock;
 	
 	//Prints out all the tracks
@@ -50,6 +53,9 @@ function displayTracks(tracks){
 	//	}
 }
 
+
+//<blockquote class="embedly-card"><h4><a href="https://www.last.fm/music/Jhen%C3%A9+Aiko/_/Sativa">Jhené Aiko - Sativa</a></h4><p>Watch the video for Sativa by Jhené Aiko for free, and see the artwork, lyrics and similar artists.</p></blockquote>
+//<script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
 
 
 
